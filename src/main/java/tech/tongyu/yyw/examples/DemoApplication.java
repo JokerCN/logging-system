5932befbd88c3950b3b2ad29ec16d317a3d827ac
JokerCN <superyangyw@126.com>
2017-06-26 13:55:22 +0800

    logging-system init slf4j spring-sleuth elastic-search

---
+++ src/main/java/tech/tongyu/yyw/examples/DemoApplication.java
@@ -0,0 +1,14 @@
+package tech.tongyu.yyw.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by admin on 2017/6/26.
+ */
+@SpringBootApplication
+public class DemoApplication {
+	public static void main(String[] args){
+		SpringApplication.run(DemoApplication.class);
+	}
+}
